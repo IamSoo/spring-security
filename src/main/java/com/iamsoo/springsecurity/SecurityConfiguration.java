807ed42cf5bd8da9aea9c167ffecbda0316045e0
Soonam <princesoonam@yahoo.co.in>
2019-12-09 22:03:11 +0800

    Modified security config

--- src/main/java/com/iamsoo/springsecurity/SecurityConfiguration.java
+++ src/main/java/com/iamsoo/springsecurity/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.iamsoo.springsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,8 +14,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("soo")
                 .password("soo")
-                .roles("USER");
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
 
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasAnyRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER")
+                .antMatchers("/").permitAll()
+                .and()
+                .formLogin();
     }
 
     @Bean
